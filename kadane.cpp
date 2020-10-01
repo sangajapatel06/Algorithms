05a46baadad592567b603d7e95e2edf9a3315c12
manan2110 <55996661+manan2110@users.noreply.github.com>
2020-10-01 22:54:05 +0530

    Update kadane.cpp
    It is algorithm to calculate Largest Sum Contiguous Subarray.

--- kadane.cpp
+++ kadane.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 int maxSubArraySum(int a[], int size) 
 { 
-	int max_so_far = INT_MIN, max_ending_here = 0; 
+	int max = INT_MIN, max_temp = 0; 
 
 	for (int i = 0; i < size; i++) 
 	{ 
-		max_ending_here = max_ending_here + a[i]; 
-		if (max_so_far < max_ending_here) 
-			max_so_far = max_ending_here; 
+		max_temp = max_temp + a[i]; 
+		if (max < max_temp) 
+			max = max_temp; 
 
-		if (max_ending_here < 0) 
-			max_ending_here = 0; 
+		if (max_temp < 0) 
+			max_temp = 0; 
 	} 
-	return max_so_far; 
+	return max; 
 } 
 
 int main() 
